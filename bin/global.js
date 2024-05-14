8279e373cae03ad21ee27933f12a135740138946
andresmpajaro <andresmpajaro@gmail.com>
2024-05-14 18:59:50 -0500

    feat: working on Issue #5. Added option to start with 'passbird start' so the user can choose between a default password or a memorable password

--- bin/global.js
+++ bin/global.js
@@ -1,9 +1,67 @@
 #!/usr/bin/env node
-let passbird = require('../src/index.js');
+import { generatePassword, generateMemorablePassword } from '../src/index.js';
+import inquirer from 'inquirer';
 
-let length = process.argv[2];
-let includeUppercase = process.argv[3] === 'true';
-let includeNumbers = process.argv[4] === 'true';
-let includeSymbols = process.argv[5] === 'true';
 
-console.log(passbird(length, includeUppercase, includeNumbers, includeSymbols));
+let questions = [
+    {
+        type: 'list',
+        name: 'passwordType',
+        message: 'What type of password do you want to generate?',
+        choices: ['Default', 'Memorable'],
+        filter: function(val) {
+            return val.toLowerCase();
+        }
+    },
+    {
+        type: 'input',
+        name: 'length',
+        message: 'How long do you want the password to be?',
+        when: function(answers) {
+            return answers.passwordType === 'default';
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'includeUppercase',
+        message: 'Include uppercase letters?',
+        default: true,
+        when: function(answers) {
+            return answers.passwordType === 'default';
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'includeNumbers',
+        message: 'Include numbers?',
+        default: true,
+        when: function(answers) {
+            return answers.passwordType === 'default';
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'includeSymbols',
+        message: 'Include symbols?',
+        default: true,
+        when: function(answers) {
+            return answers.passwordType === 'default';
+        }
+    },
+    {
+        type: 'input',
+        name: 'numWords',
+        message: 'How many words do you want in your password?',
+        when: function(answers) {
+            return answers.passwordType === 'memorable';
+        }
+    }
+];
+
+inquirer.prompt(questions).then(answers => {
+    if (answers.passwordType === 'default') {
+        console.log(generatePassword(answers.length, answers.includeUppercase, answers.includeNumbers, answers.includeSymbols));
+    } else {
+        console.log(generateMemorablePassword(answers.numWords));
+    }
+});
--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
-function generatePassword(length, includeUppercase, includeNumbers, includeSymbols) {
-    const lowercase = 'abcdefghijklmnopqrstuvwxyz';
-    const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const numbers = '0123456789';
-    const symbols = '!@#$%^&*()_-+=|{}[]:;<>,.?/';
+export function generatePassword(length, includeUppercase, includeNumbers, includeSymbols) {    const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numbers = '0123456789';
+const symbols = '!@#$%^&*()_-+=|{}[]:;<>,.?/';
 
-    let characters = lowercase;
-    if (includeUppercase) characters += uppercase;
-    if (includeNumbers) characters += numbers;
-    if (includeSymbols) characters += symbols;
+let characters = lowercase;
+if (includeUppercase) characters += uppercase;
+if (includeNumbers) characters += numbers;
+if (includeSymbols) characters += symbols;
 
-    let password = '';
-    for (let i = 0; i < length; i++) {
-        password += characters[Math.floor(Math.random() * characters.length)];
-    }
-
-    return password;
+let password = '';
+for (let i = 0; i < length; i++) {
+    password += characters[Math.floor(Math.random() * characters.length)];
 }
 
-module.exports = generatePassword;
+return password;
+}
+
+const words = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew'];
+
+export function generateMemorablePassword(numWords) {    
+    let password = '';
+    for (let i = 0; i < numWords; i++) {
+        password += words[Math.floor(Math.random() * words.length)];
+        if (i < numWords - 1) password += ' ';
+    }
+    return password;
+}
